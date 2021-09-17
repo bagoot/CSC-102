1696f797347cb8ac6a7eadd2716c605a7cb5cae0
bagoot <48165005+bagoot@users.noreply.github.com>
2021-09-17 13:15:44 -0700

    Add files via upload

---
+++ week 4/login.js
@@ -0,0 +1,20 @@
+function checkcreds(){
+  var firstName = document.getElementById("fName").value;
+  var lastName = document.getElementById("lName").value;
+  var badgeNumber= document.getElementById("badgeID").value;
+  var fullName = firstName + " "+ lastName;
+
+  console.log("first name is :" + firstName + " , last name is:" + lastName + ", badgeNumber is" + badgeNumber);
+  if (fullName > 20){
+      document.getElementById("loginStatus").innerHTML = "full name has to many charecters please try again"
+  }
+  else if(badgeNumber > 999){
+    document.getElementById("loginStatus").innerHTML = "invalid badge number try again"
+  }
+  else{ 
+      alert("acsess granted");
+      location.replace("UATSpace.html");
+  }
+
+
+}
