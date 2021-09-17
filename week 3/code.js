58a9cfe10243ebc3c856ef6b044cd0694ad82a21
bagoot <48165005+bagoot@users.noreply.github.com>
2021-09-17 11:25:31 -0700

    Add files via upload

---
+++ week 3/code.js
@@ -0,0 +1,30 @@
+function countdown2(){
+    console.log("countdown started")
+    var time = 50;
+for( var i= 0;i < 51; i++){
+    console.log(i)
+    if (i== 50)
+   { 
+        setTimeout (function() {
+        document.getElementById("countdownStatus").innerHTML= "blastoff";
+    ;
+        },50000)
+    } else if (i >25){   
+    setTimeout(function(){
+         document.getElementById("countdownStatus").innerHTML="Warning Less than ½ way to launch, time left =" +time;
+        time= time-1;
+},1000*i);} else {
+    setTimeout(function(){
+        document.getElementById("countdownStatus").innerHTML= time;
+        time= time-1;
+    },1000 * i)
+
+}
+
+
+
+     
+
+ 
+}
+}
